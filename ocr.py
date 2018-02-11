f89ce8fa1713d01d36274dc70c9ea3c1ade8ac3a
HanLinAung88 <hanlaung@stanford.edu>
2018-02-11 00:35:26 -0800

    Removed unnecessary code for command line

--- ocr.py
+++ ocr.py
@@ -12,20 +12,16 @@ import os
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image to be OCR'd")
-ap.add_argument("-p", "--preprocess", type=str, default="thresh",
-	help="type of preprocessing to be done")
 args = vars(ap.parse_args())
 
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# check to see if we should apply thresholding to preprocess the
-# image
+# apply preprocessing for thresholding to image
 gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-# make a check to see if median blurring should be done to remove
-# noise
+# apply preprocessing for noise
 gray = cv2.medianBlur(gray, 3)
 
 # write the grayscale image to disk as a temporary file so we can
